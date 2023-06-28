1cff394c0fa74d99df800ec1cf84eb480a1552fd
Umakant Dwivedi <78226651+ukdgit@users.noreply.github.com>
2023-06-28 18:59:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,19 @@
 const express = require("express");
 const path = require("path");
 const threeSum = require("./script");
-const app = express();
-// Parse JSON bodies (as sent by API clients)
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname));
 
+// Parse JSON bodies (as sent by API clients)
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/main.html"));
 });
 
-app.post("/threesum", (req, res) => {
+app.post("/threeSum", (req, res) => {
   const { arr, target } = req.body;
   const ans = threeSum(arr, target);
   res.send({ message: ans });
